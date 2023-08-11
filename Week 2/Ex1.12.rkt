7a4519ae83fcd35c6b833dde271da0d7f7c7e35f
Ishan <63498055+ishanpr@users.noreply.github.com>
2023-08-11 22:32:50 +0530

    Update Ex1.12.rkt
    Some notes

--- Week 2/Ex1.12.rkt
+++ Week 2/Ex1.12.rkt
@@ -1,4 +1,9 @@
 #lang scheme
+
+;Row 1 is the row at the top of the triangle
+;Column 1 is the left-most number in a row
+;Column n is the nth number (or right-most number) in row n
+
  (define (pascal r c) 
    (cond ((= c 1)1)
          ((= c r)1)

8abd4709fa1eceb1e6b32c86fbc3023fedb89f51
Ishan <63498055+ishanpr@users.noreply.github.com>
2023-08-12 10:56:06 +0530

    Update Ex1.12.rkt
    Updated with a recursive procedure using or statements

--- Week 2/Ex1.12.rkt
+++ Week 2/Ex1.12.rkt
@@ -1,14 +1,19 @@
 #lang scheme
 
-;Row 1 is the row at the top of the triangle
-;Column 1 is the left-most number in a row
-;Column n is the nth number (or right-most number) in row n
+;This implementation is correct but I think I copied it from somewhere...
+ ;(define (pascal r c) 
+ ;  (cond ((= c 1)1)
+ ;        ((= c r)1)
+ ;        ((> c r)0) 
+ ;      ((+ (pascal (- r 1) (- c 1)) (pascal (- r 1) c)))))
 
- (define (pascal r c) 
-   (cond ((= c 1)1)
-         ((= c r)1)
-         ((> c r)0) 
-       ((+ (pascal (- r 1) (- c 1)) (pascal (- r 1) c))))) 
-  
+(define (pascal r c)
+  (cond ((or (< r 1) (< c 1) (> c r))0)
+        ((or (= c 1) (= c r))1)
+        (else (+ (pascal (- r 1) (- c 1))
+           (pascal (- r 1) c)))))
 
+; In the second implementation, the use of the or expression within the cond structure contributed to the need for an explicit else condition.
+; The or expression introduces non-mutually-exclusive conditions, which means that more than one condition can evaluate to true for the same input, making it necessary to provide a final fallback case using else.
+; In the first implementation, each condition handles a specific case that doesn't overlap with the other conditions, therefore there is no need for an explicit else condition to catch other cases.
   

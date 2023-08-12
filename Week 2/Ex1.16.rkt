657f1a1347617ca515f94a93a2d38ac0e58ebb63
Ishan <63498055+ishanpr@users.noreply.github.com>
2023-08-12 15:11:46 +0530

    Update Ex1.16.rkt
    Added comments

--- Week 2/Ex1.16.rkt
+++ Week 2/Ex1.16.rkt
@@ -6,3 +6,7 @@
               ((even? n)(expt-iter (* b b)(/ n 2) a))
               (else (expt-iter b (- n 1) (* a b)))))
     (expt-iter b n 1))
+
+; Here when n is an even number it gets divided by 2 until it becomes 1. Once it's 1, since 1 is an odd number, the else clause is executed.
+
+; Defining and using (square b) instead of (* b b) is a better practice. (Better clarity, modularity, etc, but it doesn't matter here.

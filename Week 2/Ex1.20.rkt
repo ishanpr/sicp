d33b8218481b17ed8c69f540e7a87d46b1066843
Ishan <63498055+ishanpr@users.noreply.github.com>
2023-08-13 07:15:09 +0530

    Update Ex1.20.rkt
    Normal order

--- Week 2/Ex1.20.rkt
+++ Week 2/Ex1.20.rkt
@@ -1,6 +1,27 @@
 #lang scheme
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
 
-;Applicative-order
+; Normal order
+
+(gcd 206 40)
+(if (= 40 0)206)..)
+(gcd 40 (remainder 206 40))
+(if (= (remainder 206 40) 0)..)
+(if (= 6 0)..)
+(gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
+(if (= (remainder 40 (remainder 206 40)) 0)..)
+(if (= 4 0)..)
+(gcd (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+(if (= (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) 0)..)
+(if (= 2 0)..)
+(gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
+(if (= (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))) 0)..)
+(if (= 0 0)..)
+
+; Applicative-order
 
 (gcd 206 40)
 (gcd 40 (remainder 206 40))

0c7666f2bd7b5ff3682ae870591c0902630bf94e
Ishan <63498055+ishanpr@users.noreply.github.com>
2023-08-16 05:28:06 +0530

    Update Ex1.26.rkt

--- Week 2/Ex1.26.rkt
+++ Week 2/Ex1.26.rkt
@@ -13,6 +13,7 @@
 ; With the implementation using square, (expmod base (/ exp 2) m) is only calulated once, and then squared.
 ; With this (Θ(n)) approach (expmod base (- exp 1) m) is calculated in each recursive call regardless of it being even or not.
 
+; expmod is Θ(log(n)) as it uses log(n) number of iterations to get a^n. By doubling the amount of steps in each squaring, we end up doing 2^log(n) = Θ(n) - sicp
 ; Louis' implementation here is to calculate (expmod base (/ exp 2) m) twice and multiplying them together.
 
 ; Significantly better explanation on jots-jottings blog.

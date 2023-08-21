b045018dfa4b310940c6429ec10a57cc879459f7
Ishan <63498055+ishanpr@users.noreply.github.com>
2023-08-21 22:09:40 +0530

    Incomplete - Trying to understand higher order

---
+++ Random/Trying to understand higher order procedures.rkt
@@ -0,0 +1,13 @@
+#lang scheme
+(define (cube x) (* x x x))
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (inc n) (+ n 1))
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(sum-cubes 3 4)

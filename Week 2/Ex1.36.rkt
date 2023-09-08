4060b05a2ab04b92164f46284a15ad9ddc2d1d2f
Ishan <63498055+ishanpr@users.noreply.github.com>
2023-09-08 23:49:08 +0530

    Ex1.36

---
+++ Week 2/Ex1.36.rkt
@@ -0,0 +1,44 @@
+#lang scheme
+
+; My initial version used runtime, but apparently thats not one of the primitives thats requested here.
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (display "next: ")
+      (display next)
+      (newline)
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (display "first-guess: ")
+  (display first-guess)
+  (newline)
+  (try first-guess))
+
+
+
+;(define runtime current-inexact-milliseconds)
+
+;(define (fixed-point f first-guess)
+;  (define (close-enough? v1 v2)
+;    (< (abs (- v1 v2))
+;       tolerance)) 
+;  (define (try guess start-time)
+;    (let ((next (f guess)))
+;      (if (close-enough? guess next)
+;          (begin
+;          (report (- (runtime) start-time))
+;          next)
+;          (try next start-time))))
+;  (try first-guess (runtime)))
+;
+;(define (report elapsed-time)
+;  (display elapsed-time)
+;  (display " *** "))
+
+(fixed-point (lambda (x) (/ (log 1000)(log x)))
+             10)

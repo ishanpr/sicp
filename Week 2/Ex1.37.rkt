96a93f31946b7dc17b37e7890312917765b75aa0
Ishan <63498055+ishanpr@users.noreply.github.com>
2023-09-09 20:36:17 +0530

    Update Ex1.37.rkt

--- Week 2/Ex1.37.rkt
+++ Week 2/Ex1.37.rkt
@@ -10,6 +10,12 @@
 ; Keep in mind that n and d here are procedures not variables.
 ; With (n i) and (d i) the values of i is substituted into the n and d procedures.
 
-(cont-frac (lambda (i) 1.0) (lambda (i) 1.0) -12)
+(cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 12)
 
 ; Recursive version
+(define (cont-frac n d k)
+  (define (recur i)
+    (if (= i 0)
+        0
+        (/ (n i) (+ (d i) (recur (- i 1))))))
+  (recur k))

447817daf9d3cb6fc2a229b26d7f90f976e85354
Ishan <63498055+ishanpr@users.noreply.github.com>
2023-09-09 11:13:33 +0530

    Ex1.37

---
+++ Week 2/Ex1.37.rkt
@@ -0,0 +1,15 @@
+#lang scheme
+(define (cont-frac n d k)
+  (define (iter i result)
+    (if (= i 0)
+        result
+        (iter (- i 1) (/ (n i) (+ (d i) result)))))
+  (iter k 0))
+
+; This starts from the bottom and keeps accumulating up.
+; Keep in mind that n and d here are procedures not variables.
+; With (n i) and (d i) the values of i is substituted into the n and d procedures.
+
+(cont-frac (lambda (i) 1.0) (lambda (i) 1.0) -12)
+
+; Recursive version
